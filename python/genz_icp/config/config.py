0a0914ddf58e5b51f9886583a2994dfc32d0840b
leedongzheo <125799135+leedongzheo@users.noreply.github.com>
2026-02-15 16:07:42 +0700

    Update config.py

--- python/genz_icp/config/config.py
+++ python/genz_icp/config/config.py
@@ -31,6 +31,6 @@ class AdaptiveThresholdConfig(BaseModel):
     
     # [THÊM MỚI] Dùng cho Adaptive
     use_adaptive_planarity: bool = True
-    adaptive_threshold_base: float = 0.03
+    adaptive_threshold_base: float = 0.04
     min_adaptive_threshold: float = 0.01
     max_adaptive_threshold: float = 0.2
