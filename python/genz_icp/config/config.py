e70fac541a4a0d2e17a68639cde554766aae5305
leedongzheo <125799135+leedongzheo@users.noreply.github.com>
2026-02-23 10:18:08 +0700

    Update config.py

--- python/genz_icp/config/config.py
+++ python/genz_icp/config/config.py
@@ -9,7 +9,7 @@ class DataConfig(BaseModel):
 
 class MappingConfig(BaseModel):
     voxel_size: Optional[float] = None
-    # map_cleanup_radius: float = 100.0
+    map_cleanup_radius: float = 100.0
     # map_cleanup_radius: float = 400
     max_points_per_voxel: int = 1
     # max_points_per_voxel: int = 20
