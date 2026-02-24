7f170ecd798a23093992e735152758c455a3f3dd
leedongzheo <125799135+leedongzheo@users.noreply.github.com>
2026-02-24 16:50:26 +0700

    Update config.py

--- python/genz_icp/config/config.py
+++ python/genz_icp/config/config.py
@@ -3,39 +3,39 @@ from typing import Optional
 
 class DataConfig(BaseModel):
     max_range: float = 100.0
-    min_range: float = 0.5
-    # min_range: float = 0.0
+    # min_range: float = 0.5
+    min_range: float = 0.0
     deskew: bool = True
 
 class MappingConfig(BaseModel):
     voxel_size: Optional[float] = None
-    map_cleanup_radius: float = 100.0
-    # map_cleanup_radius: float = 400
-    max_points_per_voxel: int = 1
-    # max_points_per_voxel: int = 20
+    # map_cleanup_radius: float = 100.0
+    map_cleanup_radius: float = 400
+    # max_points_per_voxel: int = 1
+    max_points_per_voxel: int = 20
     desired_num_voxelized_points: int = 2000
 
 class RegistrationConfig(BaseModel):
     # max_num_iterations: int = 100
-    max_num_iterations: int = 150
-    # max_num_iterations: int = 500
+    # max_num_iterations: int = 150
+    max_num_iterations: int = 500
     convergence_criterion: float = 0.0001
     
     # [THÊM MỚI] Chế độ chạy:
     # 0: Hybrid (Adaptive) - Default
     # 1: Point-to-Point Only
     # 2: Point-to-Plane Only
-    registration_mode: int = 1
+    registration_mode: int = 0
 
 class AdaptiveThresholdConfig(BaseModel):
     initial_threshold: float = 2.0
     min_motion_th: float = 0.1
     
     # [CŨ] Dùng cho Baseline
-    planarity_threshold: float = 0.1
+    planarity_threshold: float = 0.17
     
     # [THÊM MỚI] Dùng cho Adaptive
     use_adaptive_planarity: bool = True
-    adaptive_threshold_base: float = 0.1
+    adaptive_threshold_base: float = 0.17
     min_adaptive_threshold: float = 0.01
     max_adaptive_threshold: float = 0.2
