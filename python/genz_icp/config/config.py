b90b16cbbd8abb02f774b8304e465e015a31c827
leedongzheo <125799135+leedongzheo@users.noreply.github.com>
2026-02-24 23:53:35 +0700

    Update config.py

--- python/genz_icp/config/config.py
+++ python/genz_icp/config/config.py
@@ -28,17 +28,17 @@ class RegistrationConfig(BaseModel):
     # 0: Hybrid (Adaptive) - Default
     # 1: Point-to-Point Only
     # 2: Point-to-Plane Only
-    registration_mode: int = 1
+    registration_mode: int = 0
 
 class AdaptiveThresholdConfig(BaseModel):
     initial_threshold: float = 2.0
     min_motion_th: float = 0.1
     
     # [CŨ] Dùng cho Baseline
-    planarity_threshold: float = 0.19
+    planarity_threshold: float = 0.002
     
     # [THÊM MỚI] Dùng cho Adaptive
     use_adaptive_planarity: bool = True
-    adaptive_threshold_base: float = 0.19
-    min_adaptive_threshold: float = 0.01
+    adaptive_threshold_base: float = 0.002
+    min_adaptive_threshold: float = 0.001
     max_adaptive_threshold: float = 0.2
